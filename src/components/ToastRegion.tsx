1dd200250a4b0b8f2d7e7539906a7c066a5620c3
COCOUVI <cocouvialexandro74@gmail.com>
2026-08-13 21:22:55 +0100

    feat:Make dynamic toast and put background in red of see all center

--- src/components/ToastRegion.tsx
+++ src/components/ToastRegion.tsx
@@ -1,6 +1,13 @@
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, cssTransition } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+const toastTransition = cssTransition({
+  enter: 'toast-enter',
+  exit: 'toast-exit',
+  collapse: true,
+  collapseDuration: 260,
+});
+
 export function ToastRegion() {
-  return <ToastContainer position="top-right" autoClose={4000} newestOnTop closeOnClick pauseOnHover theme="light" />;
+  return <ToastContainer position="top-right" autoClose={4000} newestOnTop closeOnClick pauseOnHover theme="light" transition={toastTransition} />;
 }
